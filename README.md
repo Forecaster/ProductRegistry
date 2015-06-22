f44f306869bdc1c2fa8c4a6b3401f1cb0584729b
Forecaster <martinek90@hotmail.com>
2015-06-22 15:34:02 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+What's this?
+
+Pay no attention to this repository. It was never here.
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+  <title>Product Registry</title>
+</head>
+<body>
+  <div>Stuff will go here</div>
+</body>
+</html>
