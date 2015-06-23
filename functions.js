ad4d7f26cecc4b43022a97b0ef144a6f1967661a
Forecaster <martinek90@hotmail.com>
2015-06-23 15:45:07 +0200

    Made timeString return "-" if timestamp is 0

--- functions.js
+++ functions.js
@@ -1,27 +1,32 @@
 function timeString(timestamp)
 {
-  if (timestamp.length == 10)
-    timestamp = timestamp * 1000;
+  if (timestamp > 0)
+  {
+    if (timestamp.length == 10)
+      timestamp = timestamp * 1000;
 
-  var date = new Date();
-  date.setTime(timestamp);
+    var date = new Date();
+    date.setTime(timestamp);
 
-  var year = date.getFullYear().toString();
-  var days = date.getDay().toString();
-  var month = (date.getMonth()+1).toString();
-  var hours = date.getHours().toString();
-  var minutes = date.getMinutes().toString();
+    var year = date.getFullYear().toString();
+    var days = date.getDay().toString();
+    var month = (date.getMonth() + 1).toString();
+    var hours = date.getHours().toString();
+    var minutes = date.getMinutes().toString();
 
-  if (days.length == 1)
-    days = "0" + days;
-  if (month.length == 1)
-    month = "0" + month;
-  if (hours.length == 1)
-    hours = "0" + hours;
-  if (minutes.length == 1)
-    minutes = "0" + minutes;
+    if (days.length == 1)
+      days = "0" + days;
+    if (month.length == 1)
+      month = "0" + month;
+    if (hours.length == 1)
+      hours = "0" + hours;
+    if (minutes.length == 1)
+      minutes = "0" + minutes;
 
-  return days + "/" + month + "/" + year + " " + hours + ":" + minutes;
+    return days + "/" + month + "/" + year + " " + hours + ":" + minutes;
+  }
+  else
+    return "-";
 }
 
 function truncateString(string, length)
