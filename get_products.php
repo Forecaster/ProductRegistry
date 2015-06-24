ce76a7821b3b53d427bef10828e19f0596fa6e3d
Forecaster <martinek90@hotmail.com>
2015-06-24 14:10:15 +0200

    This commit fixes date search, previous commit fixed clicking delete button opening product info dialogue

--- get_products.php
+++ get_products.php
@@ -6,10 +6,10 @@ require_once "inc_functions.php";
 if (isset($_POST['from']))
   $from = $_POST['from'];
 if (isset($_POST['to']))
-  $to = $_POST['to'];
+  $to = $_POST['to'] + (60*60*24); //Add 24 hours worth of seconds to "to" value
 
 if (isset($from) && isset($to) && $from > 0 && $to > 0)
-  $query = "SELECT * FROM products WHERE added > $from AND added < $to";
+  $query = "SELECT * FROM products WHERE added > '$from' AND added < '$to'";
 else
   $query = "SELECT * FROM products";
 
@@ -18,7 +18,10 @@ $result = mysqli_query($con, $query);
 if ($result)
 {
   if (mysqli_num_rows($result) == 0)
+  {
+    log_file("[get_products.php] Query \"$query\" returned 0 results", "sql.log");
     echo "false;noData";
+  }
   else
     echo csv(prepareForCsv($result));
 }
